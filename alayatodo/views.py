230fc74a6ce36812454ff2d73594f96a59f04679
DESKTOP-B5P8920\mazur <mazur.stefan@gmail.com>
2017-07-14 22:08:18 -0400

    TASK-1:Make description required and also check from server side to cover for browsers that don't respect required fields

--- alayatodo/views.py
+++ alayatodo/views.py
@@ -4,7 +4,8 @@ from flask import (
     redirect,
     render_template,
     request,
-    session
+    session,
+    flash
     )
 
 
@@ -65,11 +66,17 @@ def todos():
 def todos_POST():
     if not session.get('logged_in'):
         return redirect('/login')
-    g.db.execute(
-        "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
-        % (session['user']['id'], request.form.get('description', ''))
-    )
-    g.db.commit()
+
+    description = request.form.get('description')
+    if len(description) > 0:
+        g.db.execute(
+            "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
+            % (session['user']['id'], request.form.get('description', ''))
+        )
+        g.db.commit()
+    else:
+        flash('You must provide a description')
+        
     return redirect('/todo')
 
 
